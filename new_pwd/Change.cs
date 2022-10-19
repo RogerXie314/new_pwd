a56512bad495f1ba1d914ae40ec01aa7a1b22b4b
RogerXie314 <xieruogeng@hotmail.com>
2022-10-19 15:23:19 +0800

    通过.net API 修改系统密码
    输入用户名和新密码，直接修改。

---
+++ new_pwd/Change.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace new_pwd
+{
+    class Change
+    {
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+        public void Change_pwd(string user_name, string pwd)
+        {
+            SecureString password = new SecureString();
+            string pass = pwd;
+            foreach (char c in pass)
+            {
+                password.AppendChar(c);
+            }
+            IntPtr pPassword = Marshal.SecureStringToGlobalAllocUnicode(password);
+            USER_INFO_1003 ui = new USER_INFO_1003();
+            ui.usri1003_password = Marshal.SecureStringToGlobalAllocUnicode(password);
+            int err = NetUserSetInfo(".", user_name, 1003, ref ui, 0);
+            Marshal.ZeroFreeGlobalAllocUnicode(pPassword);
+            Console.Read();
+        }
+        [DllImport("Netapi32.dll")]
+        extern public static int NetUserSetInfo(
+        [MarshalAs(UnmanagedType.LPWStr)] string servername,
+        [MarshalAs(UnmanagedType.LPWStr)] string username,
+        int level,
+        ref USER_INFO_1003 buf,
+        int error);
+
+        public struct USER_INFO_1003
+        {
+            public IntPtr usri1003_password;
+        }
+    }
+}
---
+++ new_pwd/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Security;
+
+
+namespace new_pwd
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var new_pwd_username = textBox1.Text;
+            var new_pwd_password = textBox2.Text;
+            Change C_pwd = new Change();
+            try
+            {
+                C_pwd.Change_pwd(new_pwd_username,new_pwd_password);
+                
+            }
+            catch (Exception)
+            { return; }
+           
+        }
+    }
+}
